75f5dc3275b9a7e6a16aa58e6d932d9beae4163d
Francisco Martinez <franciskvolquez@hotmail.com>
2017-04-22 18:44:15 -0400

    Move DateTime calculation

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GigHub.Models;
 using System.Collections.Generic;
+using System;
 
 namespace GigHub.ViewModels
 {
@@ -10,5 +11,12 @@ namespace GigHub.ViewModels
         public string Time { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime {
+            get
+            {
+                return DateTime.Parse($"{Date} {Time}");
+            }
+        }
     }
 }
